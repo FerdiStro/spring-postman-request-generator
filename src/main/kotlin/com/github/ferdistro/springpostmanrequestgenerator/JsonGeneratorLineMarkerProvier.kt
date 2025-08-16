c2ca2fd13da20af7cb40b3610a6fb6ac57fd2d29
alex_let <alexlet@dtechs.io>
2025-08-16 17:51:13 +0300

    major refactor

--- src/main/kotlin/com/github/ferdistro/springpostmanrequestgenerator/JsonGeneratorLineMarkerProvier.kt
+++ src/main/kotlin/com/github/ferdistro/springpostmanrequestgenerator/JsonGeneratorLineMarkerProvier.kt
@@ -8,28 +8,19 @@ import com.intellij.openapi.util.IconLoader
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiMethod
 import com.intellij.psi.PsiTypeElement
-import org.jetbrains.annotations.NotNull
 import java.awt.event.MouseEvent
 import javax.swing.Icon
 
 private const val ANNOTATION_NAME = "org.springframework.web.bind.annotation.RequestMapping"
 
 class JsonGeneratorLineMarkerProvider : LineMarkerProvider {
-    val icon: Icon = IconLoader.getIcon("/META-INF/icon.svg", JsonGeneratorLineMarkerProvider::class.java)
-
-    val url: String = "{{PROTOCOL}}{{SERVER}}"
-    val urlPath: String = "{{APP_CONTEXT}}}"
-
+    private val icon: Icon = IconLoader.getIcon("/META-INF/icon.svg", JsonGeneratorLineMarkerProvider::class.java)
+    private val baseUrl: String = "{{PROTOCOL}}{{SERVER}}"
+    private val appContext: String = "{{APP_CONTEXT}}"
 
     override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? {
-        if (element is PsiMethod) {
-            if (!element.hasAnnotation(ANNOTATION_NAME)) {
-                return null
-            }
-            val modifiers = element.modifierList
-
-            val anchor = modifiers.nextSibling ?: element
-
+        if (element is PsiMethod && element.hasAnnotation(ANNOTATION_NAME)) {
+            val anchor = element.modifierList.nextSibling ?: element
             val finalAnchor = anchor as? PsiTypeElement ?: (element.nameIdentifier ?: element)
 
             return LineMarkerInfo(
@@ -44,30 +35,17 @@ class JsonGeneratorLineMarkerProvider : LineMarkerProvider {
         return null
     }
 
-
     private fun generateJson(method: PsiMethod) {
-        val stringBuilder: StringBuilder = StringBuilder()
+        val stringBuilder = StringBuilder()
 
-        val item = Item(
-            name = "",
-            request = Request(
-                method = "", header = emptyList(), url = URL(
-                    raw = "", host = emptyList(), path = emptyList(), query = emptyList()
-                )
-            ), response = emptyList()
-        )
-
-        val request = item.request
-
+        val queryItems = mutableListOf<QueryItem>()
         for (param in method.parameterList.parameters) {
-
             stringBuilder.append("Parameter: ${param.name}; Type: ${param.type.presentableText}")
-
-            request.url.query += QueryItem(key = param.name, value = "")
+            queryItems.add(QueryItem(key = param.name, value = ""))
 
             val annotations = param.modifierList?.annotations ?: emptyArray()
             for (ann in annotations) {
-                val annQualifiedName = ann.qualifiedName ?: "Unbekannt"
+                val annQualifiedName = ann.qualifiedName ?: "Unknown"
                 stringBuilder.append("  Annotation: $annQualifiedName")
 
                 val attributes = ann.parameterList.attributes
@@ -79,37 +57,89 @@ class JsonGeneratorLineMarkerProvider : LineMarkerProvider {
             }
         }
 
-        val annotation = method.modifierList.findAnnotation(ANNOTATION_NAME)
+        val annotationData = extractAnnotationData(method, stringBuilder)
 
-        if (annotation != null) {
-            val attributes = annotation.parameterList.attributes
-            for (attr in attributes) {
-                val name = attr.name ?: "value"
-                val value = attr.value?.text ?: "null"
+        val urlComponents = buildUrlComponents(annotationData.path, queryItems)
 
-
-                if (name == "value") {
-                    item.name = value.split("/").last().replace("\"", "")
-                    request.url = URL(
-                        raw = (this.url + "/" + this.urlPath + value).replace("\"", ""),
-                        host = listOf(this.url),
-                        path = (listOf(this.urlPath) + value.replace("\"", "").split("/").drop(1)),
-                        query = request.url.query
-                    )
-                }
-
-                if (name == "method") {
-                    request.method = value.split(".")[1].replace("\"", "")
-                }
-
-
-                stringBuilder.append("\"$name\" : \"$value\",")
-            }
-        }
+        val item = Item(
+            name = annotationData.name,
+            request = Request(
+                method = annotationData.method,
+                header = emptyList(),
+                url = urlComponents
+            ),
+            response = emptyList()
+        )
 
         val project = method.project
         val service = project.getService(PermanentCache::class.java)
         service.addRequest(item)
     }
-}
 
+    private data class AnnotationData(
+        val name: String,
+        val method: String,
+        val path: String
+    ) {
+        companion object {
+            val EMPTY = AnnotationData("", "", "")
+        }
+    }
+
+    private fun extractAnnotationData(
+        method: PsiMethod,
+        stringBuilder: StringBuilder
+    ): AnnotationData {
+        val annotation = method.modifierList.findAnnotation(ANNOTATION_NAME)
+            ?: return AnnotationData.EMPTY
+
+        var name = ""
+        var method = ""
+        var path = ""
+
+        val attributes = annotation.parameterList.attributes
+        for (attr in attributes) {
+            val attrName = attr.name ?: "value"
+            val attrValue = attr.value?.text ?: "null"
+
+            when (attrName) {
+                "value" -> {
+                    name = attrValue.split("/").last().replace("\"", "")
+                    path = attrValue.replace("\"", "")
+                }
+
+                "method" -> {
+                    method = attrValue.split(".").getOrElse(1) { "" }.replace("\"", "")
+                }
+            }
+
+            stringBuilder.append("\"$attrName\" : \"$attrValue\",")
+        }
+
+        return AnnotationData(name, method, path)
+    }
+
+    private fun buildUrlComponents(
+        path: String,
+        queryItems: List<QueryItem>
+    ): URL {
+        val rawUrl = if (path.isNotEmpty()) {
+            ("$baseUrl/$appContext$path").replace("\"", "")
+        } else {
+            ("$baseUrl/$appContext").replace("\"", "")
+        }
+
+        val pathComponents = if (path.isNotEmpty()) {
+            listOf(appContext) + path.replace("\"", "").split("/").drop(1)
+        } else {
+            listOf(appContext)
+        }
+
+        return URL(
+            raw = rawUrl,
+            host = listOf(baseUrl),
+            path = pathComponents,
+            query = queryItems
+        )
+    }
+}
